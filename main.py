6b2c56e6045690d86a137ab75f1508d4002820f2
MelnykIhor <melnyk70@gmail.com>
2024-10-16 19:30:46 +0300

    DZ_7_1

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+# ДЗ 7.1. Вітання
+# Написати функцію say_hi, яка представить людину за переданими параметрами.
+# Вхідні дані: Два аргументи рядок(str) та позитивне число(int)
+# Функція має повернути рядок.
+# Замініть pass на Ваше рішення.
+# def say_hi(name, age):
+#   pass
+# assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+# assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+# print('ОК')
 def say_hi(name, age):
   pass
 
