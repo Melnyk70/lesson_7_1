afeb38b61559622326c5ca311576dea7e30b290e
MelnykIhor <melnyk70@gmail.com>
2024-10-16 19:27:54 +0300

    DZ_7_1

---
+++ main.py
@@ -0,0 +1,6 @@
+def say_hi(name, age):
+  pass
+
+assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+print('ОК')
